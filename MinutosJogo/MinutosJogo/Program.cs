a774052f685ad1c2470df46cfa3620d372e401e8
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 15:06:11 -0300

    Uri 1047 MInutos Jogo

---
+++ MinutosJogo/MinutosJogo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace MinutosJogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int HoraInicio  = int.Parse(valores[0]);
+            int MinutoInicio  = int.Parse(valores[1]);
+            int HoraFinal  = int.Parse(valores[2]);
+            int MinutoFinal  = int.Parse(valores[3]);
+
+            int Inicio = HoraInicio * 60 + MinutoInicio;
+            int Final = HoraFinal * 60 + MinutoFinal;
+
+            int TempoJogo;
+
+            if( Inicio < Final)
+            {
+                TempoJogo = Final - Inicio;
+            }
+            else
+            {
+                TempoJogo = (24 * 60 - Inicio) + Final;
+            }
+
+            int duracaoHoras = TempoJogo / 60;
+            int duracaoMinutos = TempoJogo % 60;
+
+            Console.WriteLine("O JOGO DUROU " + duracaoHoras + " HORA(S) E " + duracaoMinutos + " MINUTO(S)");
+           
+        }
+    }
+}
